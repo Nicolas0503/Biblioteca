7e642d27b8f15f5b93c7bca8372ab40e17df1de5
Nicolas <jubilevc@gmail.com>
2025-05-13 14:20:50 -0300

    vdsvsdvs

--- Biblioteca/Exemplar.Designer.cs
+++ Biblioteca/Exemplar.Designer.cs
@@ -30,17 +30,33 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
             panel1 = new Panel();
+            label18 = new Label();
+            label17 = new Label();
+            label16 = new Label();
             textBox7 = new TextBox();
             numericUpDown3 = new NumericUpDown();
             listBox1 = new ListBox();
             checkBox1 = new CheckBox();
-            textBox6 = new TextBox();
+            textBoxISBN = new TextBox();
             comboBox2 = new ComboBox();
             numericUpDown2 = new NumericUpDown();
             tabPage2 = new TabPage();
+            label12 = new Label();
+            label11 = new Label();
+            numericUpDown5 = new NumericUpDown();
+            numericUpDown4 = new NumericUpDown();
             tabPage3 = new TabPage();
+            label14 = new Label();
+            textBox8 = new TextBox();
+            label13 = new Label();
+            numericUpDown6 = new NumericUpDown();
             tabPage4 = new TabPage();
+            label15 = new Label();
+            comboBox3 = new ComboBox();
             groupBox1 = new GroupBox();
             label7 = new Label();
             label6 = new Label();
@@ -51,16 +67,23 @@
             label1 = new Label();
             comboBox1 = new ComboBox();
             numericUpDown1 = new NumericUpDown();
-            textBox5 = new TextBox();
             textBox4 = new TextBox();
             textBox3 = new TextBox();
             textBox2 = new TextBox();
             textBox1 = new TextBox();
+            button1 = new Button();
+            comboBox4 = new ComboBox();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).BeginInit();
+            tabPage4.SuspendLayout();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
@@ -79,9 +102,12 @@
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(label10);
+            tabPage1.Controls.Add(label9);
+            tabPage1.Controls.Add(label8);
             tabPage1.Controls.Add(panel1);
             tabPage1.Controls.Add(checkBox1);
-            tabPage1.Controls.Add(textBox6);
+            tabPage1.Controls.Add(textBoxISBN);
             tabPage1.Controls.Add(comboBox2);
             tabPage1.Controls.Add(numericUpDown2);
             tabPage1.Location = new Point(4, 29);
@@ -93,8 +119,40 @@
             tabPage1.ToolTipText = "gtdgdg";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(30, 168);
+            label10.Name = "label10";
+            label10.Size = new Size(41, 20);
+            label10.TabIndex = 16;
+            label10.Text = "ISBN";
+            label10.Click += label10_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(30, 110);
+            label9.Name = "label9";
+            label9.Size = new Size(98, 20);
+            label9.TabIndex = 15;
+            label9.Text = "Tipo de Capa";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(30, 39);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 20);
+            label8.TabIndex = 14;
+            label8.Text = "Título";
+            // 
             // panel1
             // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label18);
+            panel1.Controls.Add(label17);
+            panel1.Controls.Add(label16);
             panel1.Controls.Add(textBox7);
             panel1.Controls.Add(numericUpDown3);
             panel1.Controls.Add(listBox1);
@@ -104,17 +162,45 @@
             panel1.TabIndex = 8;
             panel1.Visible = false;
             // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(43, 150);
+            label18.Name = "label18";
+            label18.Size = new Size(35, 20);
+            label18.TabIndex = 16;
+            label18.Text = "URL";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(39, 79);
+            label17.Name = "label17";
+            label17.Size = new Size(69, 20);
+            label17.TabIndex = 15;
+            label17.Text = "Tamanho";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(43, 7);
+            label16.Name = "label16";
+            label16.Size = new Size(65, 20);
+            label16.TabIndex = 14;
+            label16.Text = "Formato";
+            label16.Click += label16_Click;
+            // 
             // textBox7
             // 
-            textBox7.Location = new Point(43, 151);
+            textBox7.Location = new Point(43, 173);
             textBox7.Name = "textBox7";
-            textBox7.Size = new Size(125, 27);
+            textBox7.Size = new Size(150, 27);
             textBox7.TabIndex = 2;
             textBox7.Visible = false;
             // 
             // numericUpDown3
             // 
-            numericUpDown3.Location = new Point(43, 89);
+            numericUpDown3.Location = new Point(43, 102);
             numericUpDown3.Name = "numericUpDown3";
             numericUpDown3.Size = new Size(150, 27);
             numericUpDown3.TabIndex = 1;
@@ -123,6 +209,7 @@
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "Portable Document Format (PDF)", "Electronic Publication (ePUB)", "Mobipocket (MOBI)", "Kindle Package Format (KPF)", "Outros" });
             listBox1.Location = new Point(43, 30);
             listBox1.Name = "listBox1";
             listBox1.Size = new Size(150, 24);
@@ -143,16 +230,17 @@
             checkBox1.AutoSizeChanged += label7_Click;
             checkBox1.Click += label7_Click;
             // 
-            // textBox6
+            // textBoxISBN
             // 
-            textBox6.Location = new Point(30, 191);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(247, 27);
-            textBox6.TabIndex = 2;
+            textBoxISBN.Location = new Point(30, 191);
+            textBoxISBN.Name = "textBoxISBN";
+            textBoxISBN.Size = new Size(247, 27);
+            textBoxISBN.TabIndex = 2;
             // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "• Capa Dura", "• Capa Flexível ou Brochura", "• Capa de Papel Cartão", "• Capa com Sobrecapa (Jacket) • Capa com Relevo", "• Capa com Verniz UV", "• Capa Metalizada", "• Capa de Tecido", "• Capa com Janela", "• Capa Transparente", "• Capa Digital" });
             comboBox2.Location = new Point(30, 133);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(247, 28);
@@ -167,6 +255,10 @@
             // 
             // tabPage2
             // 
+            tabPage2.Controls.Add(label12);
+            tabPage2.Controls.Add(label11);
+            tabPage2.Controls.Add(numericUpDown5);
+            tabPage2.Controls.Add(numericUpDown4);
             tabPage2.Location = new Point(4, 29);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
@@ -175,8 +267,44 @@
             tabPage2.Text = "Revista";
             tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(78, 129);
+            label12.Name = "label12";
+            label12.Size = new Size(54, 20);
+            label12.TabIndex = 15;
+            label12.Text = "Edição";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(78, 230);
+            label11.Name = "label11";
+            label11.Size = new Size(59, 20);
+            label11.TabIndex = 14;
+            label11.Text = "Páginas";
+            // 
+            // numericUpDown5
+            // 
+            numericUpDown5.Location = new Point(78, 253);
+            numericUpDown5.Name = "numericUpDown5";
+            numericUpDown5.Size = new Size(150, 27);
+            numericUpDown5.TabIndex = 1;
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Location = new Point(78, 152);
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(150, 27);
+            numericUpDown4.TabIndex = 0;
+            // 
             // tabPage3
             // 
+            tabPage3.Controls.Add(label14);
+            tabPage3.Controls.Add(textBox8);
+            tabPage3.Controls.Add(label13);
+            tabPage3.Controls.Add(numericUpDown6);
             tabPage3.Location = new Point(4, 29);
             tabPage3.Name = "tabPage3";
             tabPage3.Size = new Size(306, 532);
@@ -184,8 +312,42 @@
             tabPage3.Text = "HQ";
             tabPage3.UseVisualStyleBackColor = true;
             // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(59, 219);
+            label14.Name = "label14";
+            label14.Size = new Size(72, 20);
+            label14.TabIndex = 16;
+            label14.Text = "Ilustrador";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(59, 242);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(150, 27);
+            textBox8.TabIndex = 15;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(59, 81);
+            label13.Name = "label13";
+            label13.Size = new Size(47, 20);
+            label13.TabIndex = 14;
+            label13.Text = "Título";
+            // 
+            // numericUpDown6
+            // 
+            numericUpDown6.Location = new Point(59, 104);
+            numericUpDown6.Name = "numericUpDown6";
+            numericUpDown6.Size = new Size(150, 27);
+            numericUpDown6.TabIndex = 1;
+            // 
             // tabPage4
             // 
+            tabPage4.Controls.Add(label15);
+            tabPage4.Controls.Add(comboBox3);
             tabPage4.Location = new Point(4, 29);
             tabPage4.Name = "tabPage4";
             tabPage4.Size = new Size(306, 532);
@@ -193,8 +355,28 @@
             tabPage4.Text = "Genérico";
             tabPage4.UseVisualStyleBackColor = true;
             // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(78, 70);
+            label15.Name = "label15";
+            label15.Size = new Size(39, 20);
+            label15.TabIndex = 14;
+            label15.Text = "Tipo";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "Jornais", "Calendários e Agendas", "Mapas e Atlas", "Postais e Cartões de Saudação", "Papéis de Presente e Material de Embalagem", "DVDs e Blu-rays", "CDs e Vinis", "K7", "Jogos de Tabuleiro e Quebra-Cabeças", "Material de Papelaria e Escritório", "Produtos Relacionados à Cultura Pop", " Audiolivros", "Outros" });
+            comboBox3.Location = new Point(78, 93);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(151, 28);
+            comboBox3.TabIndex = 0;
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+            // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(comboBox4);
             groupBox1.Controls.Add(label7);
             groupBox1.Controls.Add(label6);
             groupBox1.Controls.Add(label5);
@@ -204,7 +386,6 @@
             groupBox1.Controls.Add(label1);
             groupBox1.Controls.Add(comboBox1);
             groupBox1.Controls.Add(numericUpDown1);
-            groupBox1.Controls.Add(textBox5);
             groupBox1.Controls.Add(textBox4);
             groupBox1.Controls.Add(textBox3);
             groupBox1.Controls.Add(textBox2);
@@ -283,10 +464,12 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Pendente", "Lido", "Emprestado", "Devolvido", "Perdido" });
             comboBox1.Location = new Point(16, 510);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(261, 28);
             comboBox1.TabIndex = 6;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // numericUpDown1
             // 
@@ -294,13 +477,6 @@
             numericUpDown1.Name = "numericUpDown1";
             numericUpDown1.Size = new Size(261, 27);
             numericUpDown1.TabIndex = 5;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(16, 441);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(261, 27);
-            textBox5.TabIndex = 4;
             // 
             // textBox4
             // 
@@ -330,11 +506,30 @@
             textBox1.Size = new Size(261, 27);
             textBox1.TabIndex = 0;
             // 
+            // button1
+            // 
+            button1.Location = new Point(82, 642);
+            button1.Name = "button1";
+            button1.Size = new Size(628, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Salvar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Items.AddRange(new object[] { "• Romance", "• Novela", "• Conto", "• Crônica", "• Poema", "• Canção", "• Drama histórico", "• Teatro de vanguarda" });
+            comboBox4.Location = new Point(16, 441);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(261, 28);
+            comboBox4.TabIndex = 14;
+            // 
             // Exemplar
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 681);
+            ClientSize = new Size(800, 719);
+            Controls.Add(button1);
             Controls.Add(groupBox1);
             Controls.Add(tabControl1);
             Name = "Exemplar";
@@ -346,6 +541,15 @@
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).EndInit();
+            tabPage4.ResumeLayout(false);
+            tabPage4.PerformLayout();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
@@ -369,18 +573,35 @@
         private Label label1;
         private ComboBox comboBox1;
         private NumericUpDown numericUpDown1;
-        private TextBox textBox5;
         private Label label7;
         private Label label6;
         private NumericUpDown numericUpDown2;
         private TabPage tabPage3;
         private TabPage tabPage4;
-        private TextBox textBox6;
+        private TextBox textBoxISBN;
         private ComboBox comboBox2;
         private CheckBox checkBox1;
         private Panel panel1;
         private TextBox textBox7;
         private NumericUpDown numericUpDown3;
         private ListBox listBox1;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label12;
+        private Label label11;
+        private NumericUpDown numericUpDown5;
+        private NumericUpDown numericUpDown4;
+        private Label label14;
+        private TextBox textBox8;
+        private Label label13;
+        private NumericUpDown numericUpDown6;
+        private Label label15;
+        private ComboBox comboBox3;
+        private Label label18;
+        private Label label17;
+        private Label label16;
+        private Button button1;
+        private ComboBox comboBox4;
     }
 }
---
+++ Biblioteca/LivroClasse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class LivroClasse : ExemplarClasse
+    {
+        public int Paginas { get; set; }
+        public string TipoCapa { get; set; }
+        public string Isbn { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} | Páginas: {Paginas} | Capa: {TipoCapa} | ISBN: {Isbn}";
+        }
+    }
+}
